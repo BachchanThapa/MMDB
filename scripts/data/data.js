07cc76649f99fced8717fc58651a72cc154944d5
Thapa-Pro <bachchan.k.t@gmail.com>
2025-02-22 20:26:21 +0100

    cleaning-cleaning

--- scripts/data/data.js
+++
@@ -1,7 +0,0 @@
-// This object will store fetched movie data
-const oData = {
-  topMovieList: [], // Will store the top 20 movies
-  isInitialized: false, // Helps to track if data is loaded
-};
-
-export default oData;
